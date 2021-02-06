8f8e66f078ef787be48acdce2fd1991c80e0be5c
HemangKeswani12 <71550818+HemangKeswani12@users.noreply.github.com>
2021-02-06 17:09:33 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,24 @@
+class Ball{
+    constructor(x, y, r) {
+        var options={
+            'restitution': 0.3,
+            'density': 1.2,
+            'friction':0.5,
+            'isStatic': false
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(this.x,this.y,this.r/2,options);
+	    World.add(world, this.body);
+    }
+    display(){
+        var ballpos = this.body.position;
+        push();
+        translate(ballpos.x,ballpos.y);
+        ellipseMode(CENTER);
+        fill(255,0,255);
+        ellipse(0,0,this.r,this.r)
+        pop();
+    }
+}
